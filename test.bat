f1e700444316bede2ceb0eb6e0205e0ccf30c0e5
Russell Wallace <russell.wallace@gmail.com>
2017-01-25 15:26:10 +0000

    gitignore

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
-MSBuild.exe ReplaceWords.sln /p:Configuration=Debug /p:Platform="Any CPU"
+"\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ReplaceWords.sln /p:Configuration=Debug /p:Platform="Any CPU"
 if errorlevel 1 goto :eof
 echo "the quick brown fox jumped over the lazy dog" >text.txt
 echo dog,cat >words.csv
